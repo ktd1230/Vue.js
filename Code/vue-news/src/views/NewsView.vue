70f9d28cf963246394c4fc868fdb69a1b75e215b
ktd1230 <ktd1230@naver.com>
2020-07-16 23:48:08 +0900

    Vue.js 완벽 가이드 - API 구현 2

--- Code/vue-news/src/views/NewsView.vue
+++ Code/vue-news/src/views/NewsView.vue
@@ -4,22 +4,45 @@
   </div>
 </template>
 
+<script
+  src="https://code.jquery.com/jquery-3.5.1.js"
+  integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc="
+  crossorigin="anonymous"></script>
 <script>
 import { fetchNewsList } from '../api/index.js';
 
 export default {
   data() {
     return {
+      // 1
       users: [],
     }
   },
   created() {
-    fetchNewsList()
+    function callAjax() {
+      return new Promise(function(resolve, reject) {
+        $.ajax({
+          url: 'https://api.hnpwa.com/v0/news/1.json',
+          success: function(data) {
+            resolve(data);
+          }
+        });
+      });
+    }
+
+    // 2
+    // fetchNewsList()
+    callAjax()
     .then(response => {
-      this.users = response.data;
+      this.users = response;
       console.log(this.users);
+      // 3
+      console.log('함수 결과', this.users)
     })
     .catch(error => console.log(error))
+
+    // 3
+    // console.log('함수 결과', this.users)
   }
 }
 </script>
