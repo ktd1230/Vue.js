b03f16c4bf55ca2f11c23c4a8dd006bcc37064f4
ktd1230 <ktd1230@naver.com>
2020-07-22 11:22:15 +0900

    Vue.js 완벽 가이드 - 리팩토링3 - Mixin

---
+++ Code/vue-news/src/views/AskView.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <list-item></list-item>
+  </div>
+</template>
+
+<script>
+import ListItem from '../components/ListItem.vue';
+// import bus from '../utils/bus.js';
+import ListMixin from '../mixins/ListMixin.js';
+
+export default {
+  components: {
+    ListItem,
+  },
+  mixins: [ListMixin],
+  computed: {
+    //... => 전개연산자 (spread operator)
+    // #3
+    // ...mapGetters([
+    //   'fetchedAsk'
+    // ]),
+    
+    // #2
+    // ...mapState({
+    //   fetchedAsk: state => state.ask
+    // }),
+    // #1
+    // fetchedAsk() {
+    //   return this.$store.state.ask;
+    // }
+  },
+//   create() {
+//     bus.$emit('start:spinner');
+//     setTimeout(() => {
+//       this.$store.dispatch('FETCH_NEWS')
+//       .then(() => {
+//         console.log('fetched');
+//         bus.$emit('end:spinner');
+//       });
+//     }, 3000);
+//   }
+}
+</script>
+
+<style>
+</style> 
---
+++ Code/vue-news/src/views/JobsView.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <list-item></list-item>
+  </div>
+</template>
+
+<script>
+import ListItem from '../components/ListItem.vue';
+// import bus from '../utils/bus.js';
+import ListMixin from '../mixins/ListMixin.js';
+
+export default {
+  components: {
+    ListItem,
+  },
+  mixins: [ListMixin],
+//   create() {
+//     bus.$emit('start:spinner');
+//     setTimeout(() => {
+//       this.$store.dispatch('FETCH_NEWS')
+//       .then(() => {
+//         console.log('fetched');
+//         bus.$emit('end:spinner');
+//       });
+//     }, 3000);
+//   }
+}
+</script>
+
+<style>
+</style> 
